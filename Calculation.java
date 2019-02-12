74ed9e777537919136fec07bbd7d8cfae27fdd24
shezan7 <46520348+shezan7@users.noreply.github.com>
2019-02-12 15:29:42 +0600

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,12 @@
+package abstractInterface;
+
+public abstract class Calculation {
+	
+	abstract double add(double n1, double n2);
+	
+	void print()
+	{
+		System.out.println("Java is Love");
+	}
+
+}
