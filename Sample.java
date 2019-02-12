2507cef0432eab44bd3b01380e98564236f00abc
shezan7 <46520348+shezan7@users.noreply.github.com>
2019-02-12 15:31:15 +0600

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,13 @@
+package abstractInterface;
+
+public class Sample extends Calculation {
+
+	@Override
+	double add(double n1, double n2) {
+		// TODO Auto-generated method stub
+		return n1+n2;
+	}
+	
+	
+
+}
