cd3bba4d759170da656ed9e28c60041d53279b81
shezan7 <46520348+shezan7@users.noreply.github.com>
2019-02-12 15:30:54 +0600

    Add files via upload

---
+++ Sample2.java
@@ -0,0 +1,31 @@
+package abstractInterface;
+
+public class Sample2 implements Calculation2 {
+
+	@Override
+	public double add(double n1, double n2) {
+		// TODO Auto-generated method stub
+		return n1+n2;
+	}
+
+	@Override
+	public double sub(double n1, double n2) {
+		// TODO Auto-generated method stub
+		return n1-n2;
+	}
+
+	@Override
+	public double mul(double n1, double n2) {
+		// TODO Auto-generated method stub
+		return n1*n2;
+	}
+
+	@Override
+	public double div(double n1, double n2) {
+		// TODO Auto-generated method stub
+		return n1/n2;
+	}
+	
+	
+
+}
