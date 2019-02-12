85275132ec93f83a4e23b727132ae6c346902771
shezan7 <46520348+shezan7@users.noreply.github.com>
2019-02-12 15:30:06 +0600

    Add files via upload

---
+++ Calculation2.java
@@ -0,0 +1,11 @@
+package abstractInterface;
+
+public interface Calculation2 {
+	
+	
+	public double add(double n1, double n2);
+	public double sub(double n1, double n2);
+	public double mul(double n1, double n2);
+	public double div(double n1, double n2);
+
+}
